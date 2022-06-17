e155c3f5813106057c1b1ede71e903b56160b498
Guillem B <guillembaracco@gmail.com>
2022-06-17 19:17:10 +0200

    iteration 1 done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,21 @@
 // Iteration #1
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const droneSchema = new Schema({
+  name: {
+    type: String,
+  },
+  propellersment: {
+    type: Number,
+  },
+  maxSpeed: {
+    type: Number,
+    min: 0,
+    max: 18
+  }
+})
+
+const Drone = mongoose.model('Drone', droneSchema);
+
+module.exports = Drone;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,30 @@
 // Iteration #1
+const mongoose = require('mongoose');
+const Drones = require('../models/Drone.model');
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+
+  mongoose
+    .connect(MONGO_URI)
+    .then((x) => {
+      console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+    });
+    
+    Drones.create(drones)
+      .then(dronesFromDB => {
+        console.log(`Created ${lab-express-drones.length} drones`);
+     
+    mongoose.connection.close(() => console.log("Disconnected from the db"));
+  })
+
+  .catch(err =>
+    console.log(`An error occurred while getting drones from the DB: ${err}`)
+  );
+
+ 
